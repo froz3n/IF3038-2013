b18ce52172d006bb2d5100390548227d92ce8b42
Aidil Syaputra <13510105@std.stei.itb.ac.id>
2013-03-23 22:32:43 +0700

    Update do.js

--- js/do.js
+++ js/do.js
@@ -1,6 +1,3 @@
-username = 'admin';
-password = 'admin';
-
 Rp(function() {
 	genComment = function(author, body) {
 		article = document.createElement('article');
@@ -36,18 +33,80 @@ Rp(function() {
 	})
 
 	Rp('#loginForm').on('submit', function(e) {
+		e.preventDefault();	
+		$(document).ready(function(){
+    			$("#error_messege").css('display', 'none');
+    			$("#username").focus();
+		});
 
-		e.preventDefault();
-		validate = function() {
-			u = Rp('#login_username').val();
-			p = Rp('#login_password').val();
-			return u == username && p == password;
-		};
+		function showMessege(){
+			$("#error_messege").css('display', 'block');
+		}
 
-		if (validate())
-			window.location.href = 'dashboard.html';
-		else
-			alert('Invalid username/password combination.');
+		function validateDataLogin(){
+			var username = document.getElementById("username").value;
+			var password = document.getElementById("password").value;
+			if(username == '' || password == ''){
+				alert("Username or Password Empty.");
+			}else{
+				showMessege();
+				setTimeout(function(){
+					doLogin(username, password);
+				}, 1000);
+			}
+		}
+	
+		var xmlHttp = createXmlHttpRequestObject();
+		// Create XMLHttpRequest
+		function createXmlHttpRequestObject(){
+			var xmlHttp;
+			if(window.ActiveXObject){
+				try{
+					xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (e)  {
+					xmlHttp = false;
+				}
+			}
+			// jika mozilla atau yang lain
+			else {
+				try{
+					xmlHttp = new XMLHttpRequest();
+				}catch (e){
+					xmlHttp = false;
+				}
+			}
+			if (!xmlHttp)
+				alert("Error creating the XMLHttpRequest object.");
+			else{
+				return xmlHttp;
+			}
+  		}
+
+		//Memanggil file *.php Secara Asingkron
+		function doLogin(username, password){
+			if (xmlHttp.readyState == 4 || xmlHttp.readyState == 0){
+				xmlHttp.open("GET", "process.user.php?u="+username+"&p="+password+"&action=login", true);
+				xmlHttp.onreadystatechange = handleServerResponse;
+				xmlHttp.send(null);
+			}
+		}
+		//di eksekusi otomati jika pesan diterima
+		function handleServerResponse() {
+			///jika rewuest complete
+			if (xmlHttp.readyState == 4){
+				if (xmlHttp.status == 200) {
+					var ajax_data = xmlHttp.responseText;
+					if(ajax_data == 'OK'){
+						window.location.href = 'dashboard.html';
+					}else{
+						document.getElementById("error_messege").innerHTML = ajax_data;
+					}
+				}
+				else {
+					alert("ERROR: " + xmlHttp.statusText);
+				}
+			}
+		}
 	});
 
 	Rp('#editTaskLink').on('click', function(e) {
